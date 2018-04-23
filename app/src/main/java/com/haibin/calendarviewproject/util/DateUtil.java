8dab2cb6639a066f9b8d8c81bd42b72abba9d73b
star <mingxing.gui@ifuifu.com>
2018-04-23 23:11:59 +0800

    View

--- app/src/main/java/com/haibin/calendarviewproject/util/DateUtil.java
+++ app/src/main/java/com/haibin/calendarviewproject/util/DateUtil.java
@@ -1,4 +1,4 @@
-package com.haibin.calendarviewproject;
+package com.haibin.calendarviewproject.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
--- app/src/main/java/com/haibin/calendarviewproject/view/CustomWeekView.java
+++ app/src/main/java/com/haibin/calendarviewproject/view/CustomWeekView.java
@@ -1,4 +1,4 @@
-package com.haibin.calendarviewproject.style;
+package com.haibin.calendarviewproject.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
